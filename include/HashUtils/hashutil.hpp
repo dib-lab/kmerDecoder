8e7243bc3928b153509b73617316d48f383e04e1
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-04-18 01:35:11 +0200

    add largeKmerHasher class

--- include/HashUtils/hashutil.hpp
+++ include/HashUtils/hashutil.hpp
@@ -24,44 +24,83 @@
 #include <sys/types.h>
 #include <string>
 #include <stdlib.h>
-#include <stdint.h>
+#include <cstdint>
 #include <stdexcept>
 #include <unordered_map>
 #include "Utils/kmer.h"
+#include <algorithm>
+#include <boost/assign.hpp>
+using namespace boost::assign;
+
 
 using namespace std;
-class Hasher{
+
+class Hasher {
 public:
-	virtual uint64_t hash(string key){return 0;};
-	virtual uint64_t hash(uint64_t key){return 0;};
-	virtual string Ihash(uint64_t key){
-		throw logic_error("Reverese Hash function is not/ cannot be implemented for this hash function.");
-	}
-	virtual Hasher* clone(){return this;};
-};
-class MumurHasher: public Hasher{
-	using Hasher::hash;
-private:
-	uint64_t seed;
-public:
-	MumurHasher(uint64_t Iseed){seed=Iseed;}
-	Hasher* clone() { return new MumurHasher(seed);}
-	uint64_t hash(string kmer);
+    virtual uint64_t hash(const string &key) { return 0; };
+
+    virtual uint64_t hash(uint64_t key) { return 0; };
+
+    virtual string Ihash(uint64_t key) {
+        throw logic_error("Reverese Hash function is not/ cannot be implemented for this hash function.");
+    }
+
+    virtual Hasher *clone() { return this; };
 };
 
-class IntegerHasher: public Hasher{
+
+class bigKmerHasher : public Hasher {
 private:
-	uint64_t mask;
-	uint64_t kSize;
+    uint64_t kSize;
+    map<char, char> m = map_list_of ('A', 'T') ('T', 'A') ('G', 'C') ('C', 'G');
+    std::hash<string> hasher;
 public:
-	IntegerHasher(uint64_t kSize);
-	Hasher* clone() { return new IntegerHasher(kSize);}
-	uint64_t hash(string key);
-	uint64_t hash(uint64_t key);
-	string Ihash(uint64_t key);
+    explicit bigKmerHasher(uint64_t kSize);
+
+    string get_canonical_kmer(const string &kmer);
+
+    uint64_t hash(const string &key) override;
+
+    uint64_t hash(uint64_t key) override;
+
+    string Ihash(uint64_t key) override {
+        throw logic_error("Reverse Hash function is not/ cannot be implemented for this hash function.");
+    }
+
+    string Ihash(const string &key) {
+        throw logic_error("Reverse Hash function is not/ cannot be implemented for this hash function.");
+    }
 };
 
-class QHasher: public Hasher{
+class MumurHasher : public Hasher {
+    using Hasher::hash;
+private:
+    uint64_t seed;
+public:
+    explicit MumurHasher(uint64_t Iseed) { seed = Iseed; }
+
+    Hasher *clone() override { return new MumurHasher(seed); }
+
+    uint64_t hash(string kmer);
+};
+
+class IntegerHasher : public Hasher {
+private:
+    uint64_t mask;
+    uint64_t kSize;
+public:
+    IntegerHasher(uint64_t kSize);
+
+    Hasher *clone() override { return new IntegerHasher(kSize); }
+
+    uint64_t hash(const string &key) override;
+
+    uint64_t hash(uint64_t key) override;
+
+    string Ihash(uint64_t key) override;
+};
+
+class QHasher : public Hasher {
 private:
     uint64_t mask;
     uint64_t kSize;
@@ -69,94 +108,107 @@ private:
     unsigned int key_remainder_bits;
 
 public:
-    QHasher(uint64_t kSize);
+    explicit QHasher(uint64_t kSize);
+
     QHasher(uint64_t kSize, int _Q);
-    Hasher* clone() { return new QHasher(kSize, Q);}
+
+    Hasher *clone() override { return new QHasher(kSize, Q); }
 
     // To set the Q if not initialized
     void set_Q(int _Q);
 
     uint64_t merge_Q_R(uint64_t &_Q, uint64_t &R);
-    void split_Q_R(uint64_t key,uint64_t &_Q,uint64_t &R);
 
-    uint64_t normal_hash(string key);
+    void split_Q_R(uint64_t key, uint64_t &_Q, uint64_t &R);
+
+    uint64_t normal_hash(const string &key);
+
     uint64_t normal_hash(uint64_t key);
+
     uint64_t normal_Ihash(uint64_t key);
 
 
+    uint64_t hash(const string &key) override;
 
-    uint64_t hash(string key);
-    uint64_t hash(uint64_t key);
-    string Ihash(uint64_t key);
+    uint64_t hash(uint64_t key) override;
+
+    string Ihash(uint64_t key) override;
 };
 
 // TwoBitsHasher Canonical
 
-class TwoBitsHasher: public Hasher{
+class TwoBitsHasher : public Hasher {
 private:
     uint64_t kSize;
 public:
-    TwoBitsHasher(uint64_t kSize);
-    Hasher* clone() { return new TwoBitsHasher(kSize);}
-    uint64_t hash(string key);
-    uint64_t hash(uint64_t key);
-    string Ihash(uint64_t key);
+    explicit TwoBitsHasher(uint64_t kSize);
+
+    Hasher *clone() override { return new TwoBitsHasher(kSize); }
+
+    uint64_t hash(const string &key) override;
+
+    uint64_t hash(uint64_t key) override;
+
+    string Ihash(uint64_t key) override;
 };
 
 
 // TwoBitsHasher nonCanonical
 
-class noncanonical_TwoBitsHasher: public TwoBitsHasher{
+class noncanonical_TwoBitsHasher : public TwoBitsHasher {
 private:
     uint64_t kSize;
 public:
 
-    noncanonical_TwoBitsHasher(uint64_t kSize): TwoBitsHasher(kSize){
+    noncanonical_TwoBitsHasher(uint64_t kSize) : TwoBitsHasher(kSize) {
         this->kSize = kSize;
     }
 
-    Hasher* clone() { return new noncanonical_TwoBitsHasher(kSize);}
-    uint64_t hash(string key);
+    Hasher *clone() override { return new noncanonical_TwoBitsHasher(kSize); }
+
+    uint64_t hash(const string &key) override;
 };
 
 
-class noncanonical_IntegerHasher : public IntegerHasher{
+class noncanonical_IntegerHasher : public IntegerHasher {
 private:
     uint64_t kSize;
     uint64_t mask;
 public:
-    noncanonical_IntegerHasher(uint64_t kSize) : IntegerHasher(kSize){
-        this->kSize=kSize;
-        this->mask=BITMASK(2*kSize);
+    explicit noncanonical_IntegerHasher(uint64_t kSize) : IntegerHasher(kSize) {
+        this->kSize = kSize;
+        this->mask = BITMASK(2 * kSize);
     }
-    Hasher* clone() { return new noncanonical_IntegerHasher(kSize);}
-    uint64_t hash(string key);
+
+    Hasher *clone() override { return new noncanonical_IntegerHasher(kSize); }
+
+    uint64_t hash(const string &key) override;
 };
 
 
 template<class hashFnType>
-class wrapperHasher: public Hasher{
+class wrapperHasher : public Hasher {
 private:
-	hashFnType fn;
-	uint64_t kSize;
+    hashFnType fn;
+    uint64_t kSize;
 public:
-	wrapperHasher(hashFnType fnn,uint64_t kSize)
-		:fn(fnn),kSize(kSize){}
-	Hasher* clone(){
-		return new wrapperHasher(fn,kSize);
-	}
-	uint64_t hash(string key){
-		return fn(kmer::str_to_canonical_int(key));
-	}
+    wrapperHasher(hashFnType fnn, uint64_t kSize)
+            : fn(fnn), kSize(kSize) {}
 
-	uint64_t hash(uint64_t key){
-		string kmerStr = kmer::int_to_str(key,kSize);
-		return fn(key);
-	}
+    Hasher *clone() override {
+        return new wrapperHasher(fn, kSize);
+    }
+
+    uint64_t hash(const string &key) {
+        return fn(kmer::str_to_canonical_int(key));
+    }
+
+    uint64_t hash(uint64_t key) override {
+        string kmerStr = kmer::int_to_str(key, kSize);
+        return fn(key);
+    }
 
 };
-
-
 
 
 #endif  // #ifndef _HASHUTIL_H_
--- src/HashUtils/hashutil.cpp
+++ src/HashUtils/hashutil.cpp
@@ -22,196 +22,199 @@
 #include "Utils/kmer.h"
 #include <iostream>
 #include <unordered_map>
+
 using namespace std;
 
 
-	//-----------------------------------------------------------------------------
-	// MurmurHash2, 64-bit versions, by Austin Appleby
+//-----------------------------------------------------------------------------
+// MurmurHash2, 64-bit versions, by Austin Appleby
 
-	// The same caveats as 32-bit MurmurHash2 apply here - beware of alignment
-	// and endian-ness issues if used across multiple platforms.
-
-
-	// 64-bit hash for 64-bit platforms
-
-	uint64_t MumurHasher::hash(string Skey)
-	{
-		const char* SkeyChar=Skey.c_str();
-		void * key=(void *)&SkeyChar;
-		int len=Skey.size();
-		const uint64_t m = 0xc6a4a7935bd1e995;
-		const int r = 47;
-
-		uint64_t h = seed ^ (len * m);
-
-		const uint64_t * data = (const uint64_t *)key;
-		const uint64_t * end = data + (len/8);
-
-		while(data != end)
-		{
-			uint64_t k = *data++;
-
-			k *= m;
-			k ^= k >> r;
-			k *= m;
-
-			h ^= k;
-			h *= m;
-		}
-
-		const unsigned char * data2 = (const unsigned char*)data;
-
-		switch(len & 7)
-		{
-			case 7: h ^= uint64_t(data2[6]) << 48;
-			case 6: h ^= uint64_t(data2[5]) << 40;
-			case 5: h ^= uint64_t(data2[4]) << 32;
-			case 4: h ^= uint64_t(data2[3]) << 24;
-			case 3: h ^= uint64_t(data2[2]) << 16;
-			case 2: h ^= uint64_t(data2[1]) << 8;
-			case 1: h ^= uint64_t(data2[0]);
-							h *= m;
-		};
-
-		h ^= h >> r;
-		h *= m;
-		h ^= h >> r;
-
-		return h;
-	}
+// The same caveats as 32-bit MurmurHash2 apply here - beware of alignment
+// and endian-ness issues if used across multiple platforms.
 
 
-	// // 64-bit hash for 32-bit platforms
-  //
-	// uint64_t HashUtil::MurmurHash64B ( const void * key, int len, unsigned int seed )
-	// {
-	// 	const unsigned int m = 0x5bd1e995;
-	// 	const int r = 24;
-  //
-	// 	unsigned int h1 = seed ^ len;
-	// 	unsigned int h2 = 0;
-  //
-	// 	const unsigned int * data = (const unsigned int *)key;
-  //
-	// 	while(len >= 8)
-	// 	{
-	// 		unsigned int k1 = *data++;
-	// 		k1 *= m; k1 ^= k1 >> r; k1 *= m;
-	// 		h1 *= m; h1 ^= k1;
-	// 		len -= 4;
-  //
-	// 		unsigned int k2 = *data++;
-	// 		k2 *= m; k2 ^= k2 >> r; k2 *= m;
-	// 		h2 *= m; h2 ^= k2;
-	// 		len -= 4;
-	// 	}
-  //
-	// 	if(len >= 4)
-	// 	{
-	// 		unsigned int k1 = *data++;
-	// 		k1 *= m; k1 ^= k1 >> r; k1 *= m;
-	// 		h1 *= m; h1 ^= k1;
-	// 		len -= 4;
-	// 	}
-  //
-	// 	switch(len)
-	// 	{
-	// 		case 3: h2 ^= ((unsigned char*)data)[2] << 16;
-	// 		case 2: h2 ^= ((unsigned char*)data)[1] << 8;
-	// 		case 1: h2 ^= ((unsigned char*)data)[0];
-	// 						h2 *= m;
-	// 	};
-  //
-	// 	h1 ^= h2 >> 18; h1 *= m;
-	// 	h2 ^= h1 >> 22; h2 *= m;
-	// 	h1 ^= h2 >> 17; h1 *= m;
-	// 	h2 ^= h1 >> 19; h2 *= m;
-  //
-	// 	uint64_t h = h1;
-  //
-	// 	h = (h << 32) | h2;
-  //
-	// 	return h;
-	// }
+// 64-bit hash for 64-bit platforms
 
-  IntegerHasher::IntegerHasher(uint64_t kSize)
-	{
-		this->kSize=kSize;
-		this->mask=BITMASK(2*kSize);
-	}
+uint64_t MumurHasher::hash(string Skey) {
+    const char *SkeyChar = Skey.c_str();
+    void *key = (void *) &SkeyChar;
+    int len = Skey.size();
+    const uint64_t m = 0xc6a4a7935bd1e995;
+    const int r = 47;
 
-	/*
-	 *   For any 1<k<=64, let mask=(1<<k)-1. hash_64() is a bijection on [0,1<<k),
-	 *   which means
-	 *     hash_64(x, mask)==hash_64(y, mask) if and only if x==y. hash_64i() is
-	 *     the inversion of
-	 *       hash_64(): hash_64i(hash_64(x, mask), mask) == hash_64(hash_64i(x,
-	 *       mask), mask) == x.
-	 */
+    uint64_t h = seed ^(len * m);
 
-	// Thomas Wang's integer hash functions. See
-	// <https://gist.github.com/lh3/59882d6b96166dfc3d8d> for a snapshot.
-	uint64_t IntegerHasher::hash(string kmer)
-	{
-		uint64_t key=kmer::str_to_canonical_int(kmer);
-		key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
-		key = key ^ key >> 24;
-		key = ((key + (key << 3)) + (key << 8)) & mask; // key * 265
-		key = key ^ key >> 14;
-		key = ((key + (key << 2)) + (key << 4)) & mask; // key * 21
-		key = key ^ key >> 28;
-		key = (key + (key << 31)) & mask;
-		return key;
-	}
-	uint64_t IntegerHasher::hash(uint64_t key)
-	{
-		key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
-		key = key ^ key >> 24;
-		key = ((key + (key << 3)) + (key << 8)) & mask; // key * 265
-		key = key ^ key >> 14;
-		key = ((key + (key << 2)) + (key << 4)) & mask; // key * 21
-		key = key ^ key >> 28;
-		key = (key + (key << 31)) & mask;
-		return key;
-	}
-	// The inversion of hash_64(). Modified from
-	// <https://naml.us/blog/tag/invertible>
-	string IntegerHasher::Ihash(uint64_t key)
-	{
-		uint64_t tmp;
+    const uint64_t *data = (const uint64_t *) key;
+    const uint64_t *end = data + (len / 8);
 
-		// Invert key = key + (key << 31)
-		tmp = (key - (key << 31));
-		key = (key - (tmp << 31)) & mask;
+    while (data != end) {
+        uint64_t k = *data++;
 
-		// Invert key = key ^ (key >> 28)
-		tmp = key ^ key >> 28;
-		key = key ^ tmp >> 28;
+        k *= m;
+        k ^= k >> r;
+        k *= m;
 
-		// Invert key *= 21
-		key = (key * 14933078535860113213ull) & mask;
+        h ^= k;
+        h *= m;
+    }
 
-		// Invert key = key ^ (key >> 14)
-		tmp = key ^ key >> 14;
-		tmp = key ^ tmp >> 14;
-		tmp = key ^ tmp >> 14;
-		key = key ^ tmp >> 14;
+    const unsigned char *data2 = (const unsigned char *) data;
 
-		// Invert key *= 265
-		key = (key * 15244667743933553977ull) & mask;
+    switch (len & 7) {
+        case 7:
+            h ^= uint64_t(data2[6]) << 48;
+        case 6:
+            h ^= uint64_t(data2[5]) << 40;
+        case 5:
+            h ^= uint64_t(data2[4]) << 32;
+        case 4:
+            h ^= uint64_t(data2[3]) << 24;
+        case 3:
+            h ^= uint64_t(data2[2]) << 16;
+        case 2:
+            h ^= uint64_t(data2[1]) << 8;
+        case 1:
+            h ^= uint64_t(data2[0]);
+            h *= m;
+    };
 
-		// Invert key = key ^ (key >> 24)
-		tmp = key ^ key >> 24;
-		key = key ^ tmp >> 24;
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
 
-		// Invert key = (~key) + (key << 21)
-		tmp = ~key;
-		tmp = ~(key - (tmp << 21));
-		tmp = ~(key - (tmp << 21));
-		key = ~(key - (tmp << 21)) & mask;
+    return h;
+}
 
-		return kmer::int_to_str(key,kSize);
-	}
+
+// // 64-bit hash for 32-bit platforms
+//
+// uint64_t HashUtil::MurmurHash64B ( const void * key, int len, unsigned int seed )
+// {
+// 	const unsigned int m = 0x5bd1e995;
+// 	const int r = 24;
+//
+// 	unsigned int h1 = seed ^ len;
+// 	unsigned int h2 = 0;
+//
+// 	const unsigned int * data = (const unsigned int *)key;
+//
+// 	while(len >= 8)
+// 	{
+// 		unsigned int k1 = *data++;
+// 		k1 *= m; k1 ^= k1 >> r; k1 *= m;
+// 		h1 *= m; h1 ^= k1;
+// 		len -= 4;
+//
+// 		unsigned int k2 = *data++;
+// 		k2 *= m; k2 ^= k2 >> r; k2 *= m;
+// 		h2 *= m; h2 ^= k2;
+// 		len -= 4;
+// 	}
+//
+// 	if(len >= 4)
+// 	{
+// 		unsigned int k1 = *data++;
+// 		k1 *= m; k1 ^= k1 >> r; k1 *= m;
+// 		h1 *= m; h1 ^= k1;
+// 		len -= 4;
+// 	}
+//
+// 	switch(len)
+// 	{
+// 		case 3: h2 ^= ((unsigned char*)data)[2] << 16;
+// 		case 2: h2 ^= ((unsigned char*)data)[1] << 8;
+// 		case 1: h2 ^= ((unsigned char*)data)[0];
+// 						h2 *= m;
+// 	};
+//
+// 	h1 ^= h2 >> 18; h1 *= m;
+// 	h2 ^= h1 >> 22; h2 *= m;
+// 	h1 ^= h2 >> 17; h1 *= m;
+// 	h2 ^= h1 >> 19; h2 *= m;
+//
+// 	uint64_t h = h1;
+//
+// 	h = (h << 32) | h2;
+//
+// 	return h;
+// }
+
+IntegerHasher::IntegerHasher(uint64_t kSize) {
+    this->kSize = kSize;
+    this->mask = BITMASK(2 * kSize);
+}
+
+/*
+ *   For any 1<k<=64, let mask=(1<<k)-1. hash_64() is a bijection on [0,1<<k),
+ *   which means
+ *     hash_64(x, mask)==hash_64(y, mask) if and only if x==y. hash_64i() is
+ *     the inversion of
+ *       hash_64(): hash_64i(hash_64(x, mask), mask) == hash_64(hash_64i(x,
+ *       mask), mask) == x.
+ */
+
+// Thomas Wang's integer hash functions. See
+// <https://gist.github.com/lh3/59882d6b96166dfc3d8d> for a snapshot.
+uint64_t IntegerHasher::hash(const string &kmer) {
+    uint64_t key = kmer::str_to_canonical_int(kmer);
+    key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
+    key = key ^ key >> 24;
+    key = ((key + (key << 3)) + (key << 8)) & mask; // key * 265
+    key = key ^ key >> 14;
+    key = ((key + (key << 2)) + (key << 4)) & mask; // key * 21
+    key = key ^ key >> 28;
+    key = (key + (key << 31)) & mask;
+    return key;
+}
+
+uint64_t IntegerHasher::hash(uint64_t key) {
+    key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
+    key = key ^ key >> 24;
+    key = ((key + (key << 3)) + (key << 8)) & mask; // key * 265
+    key = key ^ key >> 14;
+    key = ((key + (key << 2)) + (key << 4)) & mask; // key * 21
+    key = key ^ key >> 28;
+    key = (key + (key << 31)) & mask;
+    return key;
+}
+
+// The inversion of hash_64(). Modified from
+// <https://naml.us/blog/tag/invertible>
+string IntegerHasher::Ihash(uint64_t key) {
+    uint64_t tmp;
+
+    // Invert key = key + (key << 31)
+    tmp = (key - (key << 31));
+    key = (key - (tmp << 31)) & mask;
+
+    // Invert key = key ^ (key >> 28)
+    tmp = key ^ key >> 28;
+    key = key ^ tmp >> 28;
+
+    // Invert key *= 21
+    key = (key * 14933078535860113213ull) & mask;
+
+    // Invert key = key ^ (key >> 14)
+    tmp = key ^ key >> 14;
+    tmp = key ^ tmp >> 14;
+    tmp = key ^ tmp >> 14;
+    key = key ^ tmp >> 14;
+
+    // Invert key *= 265
+    key = (key * 15244667743933553977ull) & mask;
+
+    // Invert key = key ^ (key >> 24)
+    tmp = key ^ key >> 24;
+    key = key ^ tmp >> 24;
+
+    // Invert key = (~key) + (key << 21)
+    tmp = ~key;
+    tmp = ~(key - (tmp << 21));
+    tmp = ~(key - (tmp << 21));
+    key = ~(key - (tmp << 21)) & mask;
+
+    return kmer::int_to_str(key, kSize);
+}
 
 // QHasher _________________________________-
 
@@ -246,7 +249,7 @@ void QHasher::split_Q_R(uint64_t key, uint64_t &Qval, uint64_t &R) {
     // cout << "Q: " << _Q << ", R: " << R << endl;
 }
 
-uint64_t QHasher::normal_hash(string kmer) {
+uint64_t QHasher::normal_hash(const string &kmer) {
     uint64_t key = kmer::str_to_canonical_int(kmer);
     key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
     key = key ^ key >> 24;
@@ -310,7 +313,7 @@ uint64_t QHasher::normal_Ihash(uint64_t key) {
 }
 
 
-uint64_t QHasher::hash(string key) {
+uint64_t QHasher::hash(const string &key) {
 //	    // cout << "hash()" << endl;
     uint64_t newHash;
     uint64_t Qval;
@@ -354,7 +357,7 @@ TwoBitsHasher::TwoBitsHasher(uint64_t kSize) {
     this->kSize = kSize;
 }
 
-uint64_t TwoBitsHasher::hash(string key) {
+uint64_t TwoBitsHasher::hash(const string &key) {
     return kmer::str_to_canonical_int(key);
 }
 
@@ -370,14 +373,13 @@ string TwoBitsHasher::Ihash(uint64_t key) {
 // _________ TwoBitsHasher
 
 
-uint64_t noncanonical_TwoBitsHasher::hash(string key) {
+uint64_t noncanonical_TwoBitsHasher::hash(const string &key) {
     return kmer::str_to_int(key);
 }
 
 
-uint64_t noncanonical_IntegerHasher::hash(string kmer)
-{
-    uint64_t key=kmer::str_to_int(kmer);
+uint64_t noncanonical_IntegerHasher::hash(const string &kmer) {
+    uint64_t key = kmer::str_to_int(kmer);
     key = (~key + (key << 21)) & mask; // key = (key << 21) - key - 1;
     key = key ^ key >> 24;
     key = ((key + (key << 3)) + (key << 8)) & mask; // key * 265
@@ -386,4 +388,29 @@ uint64_t noncanonical_IntegerHasher::hash(string kmer)
     key = key ^ key >> 28;
     key = (key + (key << 31)) & mask;
     return key;
+}
+
+// _________ bigKmerHasher
+
+
+uint64_t bigKmerHasher::hash(uint64_t key) {
+    return Hasher::hash(key);
+}
+
+bigKmerHasher::bigKmerHasher(uint64_t kSize) {
+    this->kSize = kSize;
+}
+
+uint64_t bigKmerHasher::hash(const string &key) {
+    return this->hasher(get_canonical_kmer(key));
+}
+
+string bigKmerHasher::get_canonical_kmer(const string &kmer) {
+    string revComp;
+    std::string::const_reverse_iterator it = kmer.crbegin();
+    while (it != kmer.crend()) {
+        revComp = revComp.append(1, m[*(it++)]);
+    };
+
+    return (kmer < revComp) ? kmer : revComp;
 }
